5b80a44ce4a7592d7b1b379107eac54a21bbd1ed
tpekar <44039373+tpekar@users.noreply.github.com>
2019-01-17 21:28:47 +0100

    Updated Data Repositories for Category, and UnitOfMeasure. Created data.sql

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,20 +1,35 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
-    //private final RecipeService recipeService;
 
-    //public IndexController(RecipeService recipeService) {
-    //    this.recipeService = recipeService;
-    //}
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(Model model) {
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getIndexPage() {
         //model.addAttribute("recipes", recipeService.getRecipes());
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        categoryOptional.ifPresent(System.out::println);
+        unitOfMeasureOptional.ifPresent(System.out::println);
 
         return "index";
     }
